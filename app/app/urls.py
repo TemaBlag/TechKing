c6918e943abb5126c01d53ecf319f22afdd7af36
TechKing <artyomblagodarniywork@gmail.com>
2025-02-06 15:03:54 +0300

    Templates for users app added

--- app/app/urls.py
+++ app/app/urls.py
@@ -28,7 +28,8 @@ app_name = 'main'
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls', namespace='main')),
-    path('catalog/', include('goods.urls', namespace='catalog'))
+    path('catalog/', include('goods.urls', namespace='catalog')),
+    path('user/', include('users.urls', namespace='user'))
 ]
 
 if settings.DEBUG:
