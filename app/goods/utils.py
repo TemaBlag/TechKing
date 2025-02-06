250caea4c416c5f6384276f828bb2bc7d256398f
TechKing <artyomblagodarniywork@gmail.com>
2025-02-06 13:19:35 +0300

    Full text search well-done!!!

--- app/goods/utils.py
+++ app/goods/utils.py
@@ -1,5 +1,11 @@
 from re import search
-from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank
+from django.db.models import Value
+from django.contrib.postgres.search import (
+    SearchVector,
+    SearchQuery,
+    SearchRank,
+    SearchHeadline,
+)
 from django.db.models import Q
 
 
@@ -9,14 +15,43 @@ from goods.models import Products
 def q_search(query):
     if query.isdigit() and len(query) <= 5:
         return Products.objects.filter(id=int(query))
-    
-    vector = SearchVector("name", "description")
-    query = SearchQuery(query)
-    
-    return Products.objects.annotate(rank=SearchRank(vector, query)).order_by("-rank")
+
+    vector = SearchVector("name", "description", config="russian")
+    query = SearchQuery(query, config="russian")
+
+    result = (
+        Products.objects.annotate(
+            rank=SearchRank(
+                vector,
+                query,
+                normalization=Value(2).bitor(Value(4)),
+            )
+        )
+        .filter(rank__gt=0)
+        .order_by("-rank")
+    )
+    result = result.annotate(
+        headline=SearchHeadline(
+            "name",
+            query,
+            start_sel='<span style="background-color: yellow;">',
+            stop_sel="</span>",
+        )
+    )
+
+    result = result.annotate(
+        bodyline=SearchHeadline(
+            "description",
+            query,
+            start_sel='<span style="background-color: yellow;">',
+            stop_sel="</span>",
+        )
+    )
+
+    return result
 
     # search by myself
-    
+
     # keywords = [word for word in query.split() if len(word) > 2]
 
     # q_objects = Q()
