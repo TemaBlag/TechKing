74722794512ccc87bf0604069be6d67ec542203f
TechKing <artyomblagodarniywork@gmail.com>
2025-02-11 18:36:05 +0300

    Add to cart view, refactor included cart templates

--- app/carts/models.py
+++ app/carts/models.py
@@ -1,3 +1,39 @@
+from statistics import quantiles
+from tabnanny import verbose
+from xml.sax.handler import property_interning_dict
+from django.contrib import sessions
 from django.db import models
 
-# Create your models here.
+from users.models import User
+from goods.models import Products
+
+class CartQueryset(models.QuerySet):
+   
+   def total_price(self):
+       return sum(cart.products_price() for cart in self)
+   
+   def total_quantity(self):
+       if self:
+           return sum(cart.quantity for cart in self)
+       return 0
+
+class Cart(models.Model):
+
+    user = models.ForeignKey(to=User, on_delete=models.CASCADE, blank=True, null=True, verbose_name='Пользователь')
+    product = models.ForeignKey(to=Products, on_delete=models.CASCADE, verbose_name='Товар')
+    quantity = models.PositiveSmallIntegerField(default=0, verbose_name='Количество')
+    session_key = models.CharField(max_length=32, blank=True, null=True)
+    created_timestamp = models.DateTimeField(auto_now_add=True, verbose_name='Дата добавления')
+
+    class Meta:
+        db_table= 'cart'
+        verbose_name = "Корзину"
+        verbose_name_plural = "Корзина"
+
+    objects = CartQueryset().as_manager()
+
+    def __str__(self):
+        return f"Корзина {self.user.username} | Товар {self.product.name} | Количество {self.quantity}"
+    
+    def products_price(self):
+        return round(self.product.sell_price() * self.quantity, 2)
